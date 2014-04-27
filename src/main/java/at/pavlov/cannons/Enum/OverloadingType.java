f13ab8778165e4f9d4fea4f320948e66db45b8c7
DerPavlov <derpavlov@gmail.com>
2014-04-27 12:46:49 +0200

    fixed formatting

--- src/main/java/at/pavlov/cannons/Enum/OverloadingType.java
+++ src/main/java/at/pavlov/cannons/Enum/OverloadingType.java
@@ -2,35 +2,26 @@ package at.pavlov.cannons.Enum;
 
 public enum OverloadingType
 {
-DISABLED(false)
-{
-	
-},
-SAFE(true)
-{
-	
-},
-REAL(true)
-{
-	
-};
-OverloadingType(boolean e)
-{
-	enabled = e;
-}
-boolean enabled;
-byte id;
-public boolean isEnabled()
-{
-	return enabled;
-}
-public static OverloadingType get(byte b)
-{
-	switch(b)
-	{
-		case 1: return OverloadingType.SAFE;
-		case 2: return OverloadingType.REAL;
-		default: return OverloadingType.DISABLED;
-	}
-}
+    DISABLED(false),
+    SAFE(true),
+    REAL(true);
+
+    OverloadingType(boolean e)
+    {
+        enabled = e;
+    }
+    boolean enabled;
+    public boolean isEnabled()
+    {
+        return enabled;
+    }
+    public static OverloadingType get(int b)
+    {
+        switch(b)
+        {
+            case 1: return OverloadingType.SAFE;
+            case 2: return OverloadingType.REAL;
+            default: return OverloadingType.DISABLED;
+        }
+    }
 }
