4d0038d707106ecc6ce544c466613a876725159c
DerPavlov <derpavlov@gmail.com>
2015-11-08 20:38:32 +0100

    added null check

--- src/main/java/at/pavlov/cannons/config/UserMessages.java
+++ src/main/java/at/pavlov/cannons/config/UserMessages.java
@@ -318,9 +318,11 @@ public class UserMessages {
 				message = message.replace("DISMANTLING_REFUND", plugin.getEconomy().format(cannon.getCannonDesign().getEconomyDestructionRefund()));
 			}
 			// show the name of the last whitelisted player
-			OfflinePlayer whiteplayer = Bukkit.getOfflinePlayer(cannon.getLastWhitelisted());
-			if (whiteplayer != null)
-				message = message.replace("LASTWHITELISTED", whiteplayer.getName());
+			if (cannon.getLastWhitelisted() != null) {
+				OfflinePlayer whiteplayer = Bukkit.getOfflinePlayer(cannon.getLastWhitelisted());
+				if (whiteplayer != null)
+					message = message.replace("LASTWHITELISTED", whiteplayer.getName());
+			}
 			// show whitelist
 			if (cannon.getWhitelist() != null){
 				List<String> names = new ArrayList<String>();
