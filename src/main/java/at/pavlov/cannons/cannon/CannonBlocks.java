f25a3eda13129afcf217bfa5d12daee9206b6b45
Intybyte <vaan.testwork@gmail.com>
2024-08-29 14:37:25 +0200

    Finish doc

--- src/main/java/at/pavlov/cannons/cannon/CannonBlocks.java
+++ src/main/java/at/pavlov/cannons/cannon/CannonBlocks.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * This class is used internally to keep track of some blocks,
  * it is not suggested to create new instances of it but instead
- * use it to get the posi
+ * use it to get the positions of the various stuff
  */
 public class CannonBlocks {
 	private Vector rotationCenter;	//center off all rotation blocks
